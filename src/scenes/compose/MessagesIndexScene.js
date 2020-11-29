cdc7af460b01a1ab5da9e8690b5af611b2ef9156
Marlon Frausto <marlonjfrausto@gmail.com>
2020-11-29 17:04:48 +0100

    removed the  sequences table and reconnected  themessages  scene. will proceed to add the edit/delete links to the table

---
+++ src/scenes/compose/MessagesIndexScene.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Text } from 'grommet';
+import { Modal, TextHero, Table } from '../../components'
+import { configureColumns, tableConfig, configureData, QUERY } from './data'
+import {
+  Switch,
+  Route,
+  useHistory,
+  useRouteMatch
+} from "react-router-dom";
+import mondaySdk from "monday-sdk-js";
+const monday = mondaySdk();
+
+export const MessagesIndexScene = ({
+  addNew
+}) => {
+
+  const history = useHistory()
+  const { url, path } = useRouteMatch()
+
+  const { REACT_APP_TOKEN: TOKEN } = process.env
+  const [sequences, setSequences] = useState({})
+  const { columns, items } = sequences
+  
+  useEffect(() => {
+    monday.setToken(TOKEN)
+    monday.api(QUERY).then((res) => {
+      setSequences(res.data.boards[0])
+    })
+  }, []);
+
+  const ModalButton = ({ label, onClick }) => (
+    <Button 
+      primary
+      label={label}
+      onClick={ onClick }
+    />
+  )
+
+  return (
+    <Switch>
+      <Route path={`${path}/new`}>
+        <Modal 
+          onClose={() => history.push(url)}
+          config={{title: 'Messages'}}
+        />
+      </Route>
+      <Route exact path="/compose/messages" >
+        <Box>
+          <TextHero
+            addNew={<ModalButton label="New Message" onClick={() => history.push(`${url}/new`)} />}
+            title="Compose"
+            subTitle="Messages"
+            body="This is the section where content for composing sequences will soon be."
+          />
+          <Box pad="medium">
+            <Table 
+              columns={(columns && configureColumns(columns, tableConfig))}
+              data={(items && configureData(items))}
+            />
+          </Box>
+        </Box>
+      </Route>
+    </Switch>
+  );
+}
--- src/scenes/compose/index.js
+++ src/scenes/compose/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Button, Heading } from "grommet";
 import { TabTemplate } from "../../components"
 import { ComposeMessages } from './ComposeMessages'
+import { MessagesIndexScene } from './MessagesIndexScene'
 import { ComposeSequences } from './ComposeSequences'
 import { configureColumns, tableConfig, configureData, QUERY } from './data'
 
@@ -9,7 +10,8 @@ export const Compose = () => {
 
   return (
       <Box pad="medium" fill="horizontal">
-        <TabTemplate />
+        {/* <TabTemplate /> */}
+        <MessagesIndexScene />
       </Box>
   )
 };
